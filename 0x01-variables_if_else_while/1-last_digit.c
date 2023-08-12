e2e00772f7cc4be6e59078f428323f9488a082c6
abnabil <abnabil.hs@gmail.com>
2023-08-12 11:20:10 +0300

    first commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,15 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
 /**
- * main - Entry point
- *
- *
- * Return: Always 0 (Sucess)
+ * main - main block
+ * Description: Print statements based on the last digit of the random number.
+ * Return: 0
  */
-
 int main(void)
 {
 	int n;
