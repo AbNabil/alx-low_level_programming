5ba0def3ae845a8787a53b5ea8446534eb3edfa5
abnabil <abnabil.hs@gmail.com>
2023-08-12 11:13:28 +0300

    first commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ int main(void)
 	printf("%s %d is %d and is ", last, n, n % 10);
 	if (n % 10 > 5)
 	{
-		printf("greater than 5\n");
+		printf("greater than 5");
 	}
 	else if (n % 10 == 0)
 	{
-		printf("0\n");
+		printf("0");
 	}
 	else
 	{
-		printf("less than 6 and not 0\n");
+		printf("less than 6 and not 0");
 	}
 	return (0);
 }
