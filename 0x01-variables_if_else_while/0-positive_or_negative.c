bc915f4f81937348efd5d6766dd146b591625575
abnabil <abnabil.hs@gmail.com>
2023-08-12 10:35:57 +0300

    first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there 
+/**
  * main - Determine if a random number is positive, negative or zero.
- */
-/* betty style doc for function main goes there 
+ *
  * Return: 0 on success
- * */
+ */
 int main(void)
 {
 	int n;
